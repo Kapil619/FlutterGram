2f03d3caea6b8e435d5479322497d08ace787582
kapil619 <kapilbadokar321@gmail.com>
2024-01-22 14:19:24 +0530

    Configured firebase in our main.dart file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttergram/firebase_options.dart';
 import 'package:fluttergram/responsive/mobile_screen_layout.dart';
 import 'package:fluttergram/responsive/responsive_layout_screen.dart';
 import 'package:fluttergram/responsive/web_screen_layout.dart';
 import 'package:fluttergram/utils/colors.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
