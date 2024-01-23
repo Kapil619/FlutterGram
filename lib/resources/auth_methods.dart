1899c15be66727e8bf87fa75e52a5f616902d6c2
kapil619 <kapilbadokar321@gmail.com>
2024-01-23 09:46:11 +0530

    FIrebase collection setup for authentication

---
+++ lib/resources/auth_methods.dart
@@ -0,0 +1,51 @@
+import "package:cloud_firestore/cloud_firestore.dart";
+import "package:firebase_auth/firebase_auth.dart";
+import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+
+class AuthMethods {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  //Signup user
+  Future<String> signupUser({
+    required String email,
+    required String password,
+    required String username,
+    required String bio,
+    // required Uint8List file,
+  }) async {
+    String res = 'Some error occured';
+    try {
+      if (email.isNotEmpty ||
+          password.isNotEmpty ||
+          username.isNotEmpty ||
+          bio.isNotEmpty) {
+        UserCredential cred = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+        //add user to db
+        await _firestore.collection('users').doc(cred.user!.uid).set({
+          'email': email,
+          'username': username,
+          'uid': cred.user!.uid,
+          'bio': bio,
+          'followers': [],
+          'following': [],
+        });
+
+        // await _firestore.collection('users').add({
+        //   'email': email,
+        //   'username': username,
+        //   'uid': cred.user!.uid,
+        //   'bio': bio,
+        //   'followers': [],
+        //   'following': [],
+        // });
+
+        res = 'Success';
+      }
+    } catch (err) {
+      res = err.toString();
+    }
+    return res;
+  }
+}
