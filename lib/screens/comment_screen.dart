87cc34cb622dd4e042d8abcf6a25e09361ad854c
kapil619 <kapilbadokar321@gmail.com>
2024-01-25 16:42:27 +0530

    Ordered comments by descending & displayed number of comments with Querysnapshot (Not Stream).

--- lib/screens/comment_screen.dart
+++ lib/screens/comment_screen.dart
@@ -40,6 +40,7 @@ class _CommentScreenState extends State<CommentScreen> {
             .collection('posts')
             .doc(widget.snap['postId'])
             .collection('comments')
+            .orderBy('datePublished', descending: true)
             .snapshots(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -1,9 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttergram/models/user.dart';
 import 'package:fluttergram/providers/user_provider.dart';
 import 'package:fluttergram/resources/firestore_methods.dart';
 import 'package:fluttergram/screens/comment_screen.dart';
 import 'package:fluttergram/utils/colors.dart';
+import 'package:fluttergram/utils/utils.dart';
 import 'package:fluttergram/widgets/like_animation.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
@@ -18,6 +20,27 @@ class PostCard extends StatefulWidget {
 
 class _PostCardState extends State<PostCard> {
   bool isLikeAnimating = false;
+  int commentLength = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    getComments();
+  }
+
+  void getComments() async {
+    try {
+      QuerySnapshot snap = await FirebaseFirestore.instance
+          .collection('posts')
+          .doc(widget.snap['postId'])
+          .collection('comments')
+          .get();
+      commentLength = snap.docs.length;
+    } catch (e) {
+      showSnackBar(e.toString(), context);
+    }
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -218,9 +241,10 @@ class _PostCardState extends State<PostCard> {
                   onTap: () {},
                   child: Container(
                     padding: const EdgeInsets.symmetric(vertical: 4),
-                    child: const Text(
-                      'View all 200 comments',
-                      style: TextStyle(fontSize: 16, color: secondaryColor),
+                    child: Text(
+                      'View all $commentLength comments',
+                      style:
+                          const TextStyle(fontSize: 16, color: secondaryColor),
                     ),
                   ),
                 ),
